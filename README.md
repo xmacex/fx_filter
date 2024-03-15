e7a9b181456ee5ba83c762aeb39deb84d1a6e5e4
Mace Ojala <mace.ojala@gmail.com>
2024-03-15 15:35:55 +0100

    Renamed the whole thing to fx_filter. Release candidate for testing.

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
 # Filter fx for norns
 
-A simple implementation of [DFM1](https://doc.sccode.org/Classes/DFM1.html) SuperCollider filter as a norns effect. Requires sixolet's amazing fx framework.
+A simple implementation of [DFM1](https://doc.sccode.org/Classes/DFM1.html) SuperCollider filter as a norns effect. Requires sixolet's amazing [fx mod](https://llllllll.co/t/fx-mod/62726) framework, updated on 2024-03-14.
 
 > DFM1 is a digitally modelled analog filter.1 It provides low-pass and high-pass filtering. The filter can be overdriven and will self-oscillate at high resonances.
 
 The norns fx caps resonance and gain for safety reasons.
+
+# Requirements
+
+- norns
+- [fx mod](https://llllllll.co/t/fx-mod/62726)
+
+# Installation
+
+After installing the fx mod and enabling it, install the filter via [Maiden](https://norns.local) with
+
+```
+;install https://github.com/xmacex/fx_filter
+```
+
+Then, enable via the `SYSTEM > MODS` menu, and restart norns.
+
+# Usage
+
+You'll find the exposed parameters in norns menu in the section `fx filter`.
+
+See the [fx mod](https://llllllll.co/t/fx-mod/62726) documentation about routing to sends, end-of-chain etc.
--- lib/mod.lua
+++ lib/mod.lua
@@ -1,25 +1,24 @@
 local fx = require("fx/lib/fx")
 local mod = require 'core/mods'
 
-local FxDfm1 = fx:new{
-    subpath = "/fx_dfm1"
+local FxFilter = fx:new{
+   subpath = "/fx_filter"
 }
 
-function FxDfm1:add_params()
-   params:add_separator("fx_dfm1", "fx dfm1")
-    FxDfm1:add_slot("fx_dfm1_slot", "slot")
-    FxDfm1:add_taper("fx_dfm1_freq", "freq", "freq", 20, 20000, 20000, 2, "Hz")
-    FxDfm1:add_taper("fx_dfm1_res", "res", "res", 0, 0.95, 0.1, 1)
-    FxDfm1:add_taper("fx_dfm1_inputgain", "inputgain", "inputgain", 0.1, 3, 1, 1, "")
-    FxDfm1:add_option("fx_dfm1_type", "type", "type", {"lp", "hp"}, 1)
-
+function FxFilter:add_params()
+   params:add_separator("fx_filter", "fx filter")
+   FxFilter:add_slot("fx_filter_slot", "slot")
+   FxFilter:add_taper("fx_filter_freq", "freq", "freq", 20, 20000, 20000, 2, "Hz")
+   FxFilter:add_taper("fx_filter_res", "res", "res", 0, 0.95, 0.1, 1)
+   FxFilter:add_taper("fx_filter_inputgain", "inputgain", "inputgain", 0.1, 3, 1, 1, "")
+   FxFilter:add_option("fx_filter_type", "type", "type", {"lp", "hp"}, 1)r
 end
 
-mod.hook.register("script_pre_init", "dfm1 mod pre init", function()
-    FxDfm1:install()
+mod.hook.register("script_pre_init", "filter mod pre init", function()
+                     FxFilter:install()
 end)
 
-mod.hook.register("script_post_cleanup", "dfm1 mod post cleanup", function()
+mod.hook.register("script_post_cleanup", "filter mod post cleanup", function()
 end)
 
-return FxDfm1
+return FxFilter
