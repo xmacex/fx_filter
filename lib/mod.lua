06890371e0755aa9a787a6b9bd6194930823acaa
Mace Ojala <mace.ojala@gmail.com>
2024-03-15 18:06:10 +0100

    Wupsie rogue r in there.

--- lib/mod.lua
+++ lib/mod.lua
@@ -11,7 +11,7 @@ function FxFilter:add_params()
    FxFilter:add_taper("fx_filter_freq", "freq", "freq", 20, 20000, 20000, 2, "Hz")
    FxFilter:add_taper("fx_filter_res", "res", "res", 0, 0.95, 0.1, 1)
    FxFilter:add_taper("fx_filter_inputgain", "inputgain", "inputgain", 0.1, 3, 1, 1, "")
-   FxFilter:add_option("fx_filter_type", "type", "type", {"lp", "hp"}, 1)r
+   FxFilter:add_option("fx_filter_type", "type", "type", {"lp", "hp"}, 1)
 end
 
 mod.hook.register("script_pre_init", "filter mod pre init", function()
